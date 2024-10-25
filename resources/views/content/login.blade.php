aaa8b1a2696f62c650a34ed3c9a18dff4c2dec1b
Muhammad Ismam Assyifa <muhammadismam31@gmail.com>
2024-10-25 15:22:06 +0700

    pembuatan UI login

---
+++ resources/views/content/login.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Warung Tikungan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+    .container {
+        display: flex;
+        margin-top: 50px;
+    }
+
+    .card {
+        width: 40%;
+        height: auto;
+        margin-left: 10px;
+    }
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+        <img src="../img/Chef.png" width="50%" alt="Chef">
+        <div class="card card-login mt-5">
+            <div class="card-body">
+                <h1 class="text-center mt-5">M A S U K</h1>
+                <form action="" method="POST">
+                    <div class="form-floating m-3 mt-5">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="form-floating m-3">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary m-3">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,3 @@
+<div class="header bg-primary">
+    <h4 class="text-center p-3">WarungTikungan</h4>
+</div>
