bb8434bd748ba6e24ac6440fbc0e97191180eb28
Muhammad Ismam Assyifa <muhammadismam31@gmail.com>
2024-11-08 22:12:00 +0700

    Menambahkan tag div pada baris 29 sampai 35

--- resources/views/content/dashboard.blade.php
+++ resources/views/content/dashboard.blade.php
@@ -26,6 +26,13 @@
                                     </div>
                                     <!-- /.info-box -->
                                 </div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
                                 <!-- /.col -->
                                 <div class="col-12 col-sm-6 col-md-6">
                                     <div class="info-box mb-3">
