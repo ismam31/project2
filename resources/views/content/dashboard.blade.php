fb395fe166a01d514934ffe2cc0bde09f4af0cc0
Muhammad Ismam Assyifa <muhammadismam31@gmail.com>
2024-11-08 22:22:43 +0700

    Hapus Div

--- resources/views/content/dashboard.blade.php
+++ resources/views/content/dashboard.blade.php
@@ -26,13 +26,6 @@
                                     </div>
                                     <!-- /.info-box -->
                                 </div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                                <div></div>
                                 <!-- /.col -->
                                 <div class="col-12 col-sm-6 col-md-6">
                                     <div class="info-box mb-3">
