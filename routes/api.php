6ac4a52573ca67d12c0da78c988298263a72a4f7
Ninda Fidyanti <nindafy575@gmail.com>
2024-12-27 10:48:06 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MenuController;
+use App\Http\Controllers\ProductController;
 
-Route::get('products', [MenuController::class, 'index']);
-Route::post('products', [MenuController::class, 'store']);
-Route::post('products/{id}', [MenuController::class, 'show']);
-Route::put('products/{id}', [MenuController::class, 'update']);
-Route::delete('products/{id}', [MenuController::class, 'destroy']);
+Route::get('products', [ProductController::class, 'index']);
+Route::post('products', [ProductController::class, 'store']);
+Route::post('products/{id}', [ProductController::class, 'show']);
+Route::put('products/{id}', [ProductController::class, 'update']);
+Route::delete('products/{id}', [ProductController::class, 'destroy']);
