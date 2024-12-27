f3928aec4431bc9b61b85d3ac425e400503f1983
Ninda Fidyanti <nindafy575@gmail.com>
2024-12-27 10:39:54 +0700

    membuat sebuah route untuk menangani semua url yang diminta

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('products', [MenuController::class, 'index']);
+Route::post('products', [MenuController::class, 'store']);
+Route::post('products/{id}', [MenuController::class, 'show']);
+Route::put('products/{id}', [MenuController::class, 'update']);
+Route::delete('products/{id}', [MenuController::class, 'destroy']);
