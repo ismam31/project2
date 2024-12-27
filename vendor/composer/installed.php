3afd3d3d398caae2ca9f49b92bb1ceda38128313
ilhamcomrade <ilhamwiguna2005@gmail.com>
2024-12-27 10:05:16 +0700

    membuat api

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '646bfb9cefd6a76aa2e4e34126c5e2d07104eef6',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9901ae2d422013eff0748a127c5939b7878b2512',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '646bfb9cefd6a76aa2e4e34126c5e2d07104eef6',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9901ae2d422013eff0748a127c5939b7878b2512',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -429,6 +429,15 @@
             'install_path' => __DIR__ . '/../laravel/sail',
             'aliases' => array(),
             'dev_requirement' => true,
+        ),
+        'laravel/sanctum' => array(
+            'pretty_version' => 'v4.0.7',
+            'version' => '4.0.7.0',
+            'reference' => '698064236a46df016e64a7eb059b1414e0b281df',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel/sanctum',
+            'aliases' => array(),
+            'dev_requirement' => false,
         ),
         'laravel/serializable-closure' => array(
             'pretty_version' => 'v2.0.0',
